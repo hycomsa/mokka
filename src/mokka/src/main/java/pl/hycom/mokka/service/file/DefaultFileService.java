fba376fa51656c931678728d0eb3a2866bcc5e70
Mariusz Krysztofowicz <mariusz.krysztofowicz@hycom.pl>
2016-07-01 09:18:19 +0200

    #3 Fetch file from local file system - fix

--- src/mokka/src/main/java/pl/hycom/mokka/service/file/DefaultFileService.java
+++ src/mokka/src/main/java/pl/hycom/mokka/service/file/DefaultFileService.java
@@ -3,34 +3,48 @@ package pl.hycom.mokka.service.file;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * @author Mariusz Krysztofowicz (mariusz.krysztofowicz@hycom.pl)
  */
-@PropertySource("classpath:application.properties")
+
+/**
+ * Service responsible for file management, implements FileService interface
+ */
 @Component
 public class DefaultFileService implements FileService {
     private static final Logger LOG = Logger.getLogger(DefaultFileService.class);
+    /**
+     * Location for files
+     */
     @Value("${file.download.directory}")
     private String sourceDirectory;
 
+    /**
+     * Gets file by given fileName contained in configured directory, if file doesn't exist throws FileNotFoundException
+     *
+     * @param fileName
+     *         String Searching file name
+     * @return File
+     * @throws FileNotFoundException
+     */
     @Override
-    public File fetchFile(String fileName) {
+    public File fetchFile(String fileName) throws FileNotFoundException {
         LOG.debug("Invoking DefaultFileService#fetchFile with argument [" + fileName + "]");
         if (StringUtils.isEmpty(fileName)) {
             LOG.warn("File name cannot be empty");
-            return null;
+            throw new FileNotFoundException();
         }
         String fullPath = sourceDirectory + fileName;
 
         File file = new File(fullPath);
         if (!file.exists()) {
             LOG.warn("File with path [" + fullPath + "] does not exist");
-            return null;
+            throw new FileNotFoundException();
         }
         LOG.debug("Found file [" + file.getAbsolutePath() + "]");
         LOG.debug("Ending DefaultFileService#fetchFile");
--- src/mokka/src/test/java/pl/hycom/mokka/service/file/DefaultFileServiceTest.java
+++ src/mokka/src/test/java/pl/hycom/mokka/service/file/DefaultFileServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import pl.hycom.mokka.AbstractTest;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * @author Mariusz Krysztofowicz (mariusz.krysztofowicz@hycom.pl)
@@ -14,15 +15,16 @@ public class DefaultFileServiceTest extends AbstractTest {
     DefaultFileService fileService = new DefaultFileService();
 
     @Test
-    public void testFetchFileSuccess() {
+    public void testFetchFileSuccess() throws FileNotFoundException {
         fileService.setSourceDirectory("src/test/resource/");
         File file = fileService.fetchFile("samplefile.txt");
         Assert.assertNotNull(file);
     }
 
-    @Test
-    public void testFetchFileFailure() {
+    @Test(expected = FileNotFoundException.class)
+    public void testFetchFileFailure() throws FileNotFoundException {
         fileService.setSourceDirectory("src/test/resource/");
+
         File file = fileService.fetchFile("samplefile1.txt");
         Assert.assertNull(file);
 
