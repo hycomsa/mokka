775c2307fd7be5b2012ded6c6752cbfabf730570
Mariusz Krysztofowicz <mariusz.krysztofowicz@hycom.pl>
2016-07-04 10:21:17 +0200

    #3 Fetch file from local file system - refactoring

--- src/mokka/src/main/java/pl/hycom/mokka/endpoint/FileController.java
+++ src/mokka/src/main/java/pl/hycom/mokka/endpoint/FileController.java
@@ -1,12 +1,12 @@
 package pl.hycom.mokka.endpoint;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +18,6 @@ import pl.hycom.mokka.service.file.FileService;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URLConnection;
-import java.util.Objects;
 
 /**
  * Rest controller responsible for file management
@@ -28,8 +26,8 @@ import java.util.Objects;
  */
 @RequestMapping(value = "/files")
 @RestController
-public class FileEndpoint {
-    private static final Logger LOG = Logger.getLogger(FileEndpoint.class);
+public class FileController {
+    private static final Logger LOG = LoggerFactory.getLogger(FileController.class);
     /**
      * FileService
      */
@@ -38,8 +36,7 @@ public class FileEndpoint {
 
     /**
      * Method returns file  with given file name wrapped with ResponseEntity,
-     * if extension argument is empty returns HttpStatus.BAD_REQUEST,
-     * if file doesn't exist returns HttpStatus.NOT_FOUND
+     * if file doesn't exist throws  FileNotFoundException
      *
      * @param fileId
      *         File name path variable
@@ -53,34 +50,20 @@ public class FileEndpoint {
     @ResponseBody
     public ResponseEntity<FileSystemResource> fetchFile(
             @PathVariable("file-id")
-            String fileId,
+                    String fileId,
             @RequestParam(name = "ext")
-            String extension) {
+                    String extension) throws FileNotFoundException, IllegalArgumentException {
 
-        String fileName = null;
-        File file;
-        try {
-            Objects.requireNonNull(extension);
-            fileName = fileId + "." + extension;
-            file = fileService.fetchFile(fileName);
-        } catch (FileNotFoundException e) {
-            LOG.warn("Could not find file with given name [" + fileName + "]", e);
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        } catch (NullPointerException e) {
-            LOG.warn("Extension cannot be null", e);
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        String fileName = fileId + "." + extension;
+        File file = fileService.fetchFile(fileName);
 
         HttpHeaders headers = new HttpHeaders();
 
         headers.add("Content-Disposition", "attachment; filename=" + file.getName());
-        String mimeType = URLConnection.guessContentTypeFromName(file.getName());
-        if (StringUtils.isNotBlank(mimeType) && MediaType.parseMediaType(mimeType) != null) {
-            headers.setContentType(MediaType.parseMediaType(mimeType));
-        }
+
         headers.setContentLength(file.length());
         FileSystemResource fileSystemResource = new FileSystemResource(file);
-        LOG.debug("Ending FileEndpoint#fetchFile with status [" + HttpStatus.OK + "]");
+        LOG.debug("Ending FileController#fetchFile with status [" + HttpStatus.OK + "]");
         return new ResponseEntity<>(fileSystemResource, HttpStatus.OK);
     }
 
