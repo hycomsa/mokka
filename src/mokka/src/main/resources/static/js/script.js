103c31b7af2b36fa31f6775802d657a7a3f96f29
Patryk Kwiatkowski <patryk.kwiatkowski@hycom.pl>
2017-08-29 14:45:39 +0200

    Filtering on key 'enter' enabled;

--- src/mokka/src/main/resources/static/js/script.js
+++ src/mokka/src/main/resources/static/js/script.js
@@ -37,6 +37,23 @@ app.filter('to_trusted', ['$sce', function($sce){
     };
 }]);
 
+/*
+This directive allows us to pass a function in on an enter key to do what we want.
+ */
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
+
 app.run(function($rootScope, $http) {
 	return $http.get('/user/current').then(function(response){
 		$rootScope.user = response.data;
